360927e450b973844f488740e091df6c1720cb7e
Yuzel Rose <zelinkmd@gmail.com>
2024-04-29 21:34:05 -0600

    versión pre-release 0.80.7

--- app/src/main/java/com/example/firamadeaccesos/QrFun.kt
+++ app/src/main/java/com/example/firamadeaccesos/QrFun.kt
@@ -2,12 +2,8 @@ package com.example.firamadeaccesos
 
 import android.content.Context
 import android.util.Log
-import android.view.View
-import android.widget.Switch
-import android.widget.TextView
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
-import kotlin.math.log
 
 class QrFun {
 
@@ -23,28 +19,15 @@ class QrFun {
     }
     /* URL ? access_key= 13_digitos Modo*/
     fun qrInf(usrIn: String, con: Context, Dbug: Boolean): Array<String> {//Obtener informacion del QR
-        val contDat = arrayOf("", "","") //URL, KEY MODO
+        val contDat = arrayOf("", "") //KEY, MOD
         try {
-            var stgURL = false
-            for (i in usrIn.indices) {
-                if (usrIn[i] == '?') {
-                    stgURL = true
-                } else {
-                    if (!stgURL) {
-                        contDat[0] = contDat[0] + usrIn[i]//Url
-                    } else {
-                        if(usrIn[i] == '='){
-                            for (x in 1..13){
-                                contDat[1] = contDat[1] + usrIn[i+x]//Key
-                            }
-                            contDat[2] = contDat[2] + usrIn[usrIn.length - 1]//Mod
-                        }
-                    }
-                }
+            for (x in 1..13){
+                contDat[0] = contDat[0] + usrIn[x]//Key
             }
+            contDat[1] = contDat[1] + usrIn[usrIn.length - 1]//Mod
             if (Dbug){
                 Log.d("QR","Resibido: $usrIn")
-                Log.d("QR", "Url: ${contDat[0]}, Key: ${contDat[1]}, Mod: ${contDat[2]}")
+                Log.d("QR", "Key: ${contDat[0]}, Mod: ${contDat[1]}")
             }
         } catch (e: Exception) {
             Toast.makeText(con, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
